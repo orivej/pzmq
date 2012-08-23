1168be69bb092c106cbd50c159b06f296399b6e0
Orivej Desh <orivej@gmx.fr>
2012-08-23 04:54:31 +0400

    Let gendocs work regardless of current working directory.

--- gendocgen.lisp
+++ gendocgen.lisp
@@ -1,7 +1,14 @@
 (require '#:pzmq)
 (load "gendocs.lisp")
+
+(defun toplevel ()
+  (require '#:pzmq)
+  (setf *default-pathname-defaults*
+        (make-pathname :name nil :type nil :defaults *core-pathname*))
+  (load "gendocs.lisp"))
+
 (sb-ext:save-lisp-and-die
  "gendocs"
- :toplevel (lambda () (require '#:pzmq) (load "gendocs.lisp"))
+ :toplevel #'toplevel
  :executable t
  :compression t)
