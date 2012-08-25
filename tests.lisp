401cfa1cf48ae24f708eca25f55244f8094a5906
Orivej Desh <orivej@gmx.fr>
2012-08-25 15:21:00 +0400

    Add response-request test.

--- tests.lisp
+++ tests.lisp
@@ -16,3 +16,10 @@
     (pzmq:connect sender "tcp://localhost:5555")
     (pzmq:send sender "")
     (is (string= "" (pzmq:recv-string receiver)))))
+
+(test response-request
+  (pzmq:with-sockets ((sender :req) (receiver :rep))
+    (pzmq:bind receiver "tcp://*:5555")
+    (pzmq:connect sender "tcp://localhost:5555")
+    (bt:make-thread (lambda () (sleep 0.01) (pzmq:send sender "")))
+    (is (string= "" (pzmq:recv-string receiver)))))
