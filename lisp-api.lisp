18a5a69016740de78d4c04485f9c881a1270cf3e
Michał "phoe" Herda <phoe@disroot.org>
2020-03-31 22:35:48 +0200

    Fix secondary retvals in docstring for RECV-{STRING,OCTETS}

--- lisp-api.lisp
+++ lisp-api.lisp
@@ -15,7 +15,8 @@
 
 (defun recv-string (socket &key dontwait (encoding cffi:*default-foreign-encoding*))
   "Receive a message part from a socket as a string.
-@return{A string.}"
+@return{A string as the primary value, and the value of the socket
+ option :rcvmore as the secondary value.}"
   (with-message msg
     (msg-recv msg socket :dontwait dontwait)
     (values
@@ -24,7 +25,8 @@
 
 (defun recv-octets (socket &key dontwait)
   "Receive a message part from a socket as an octet vector.
-@return{A (vector (unsigned-byte 8)).}"
+@return{A (vector (unsigned-byte 8)) as the primary value, and the value
+ of the socket option :rcvmore as the secondary value.}"
   (with-message msg
     (msg-recv msg socket :dontwait dontwait)
     (values
